9b4e236e419736f2fa6302098d23c90f6783798d
Kamogelo <ekmogasoa@gmail.com>
2024-12-05 16:28:41 +0200

    fixed the conditional logic

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const dataRes = data("john", 30, "Help-Desk", "mike", "ben", "tim");
 
 if (
   dataRes instanceof Object &&
-  Array.isArray(dataRes.names || dataRes instanceof Array)
+  (Array.isArray(dataRes.names) || dataRes instanceof Array)
 ) {
   const results = { ...dataRes };
   console.log(results);
