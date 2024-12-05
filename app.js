62450278c54fe878eaf9701942ec7ba2aa05edd5
Kamogelo <ekmogasoa@gmail.com>
2024-12-05 16:22:34 +0200

    added spread operator to pull display the whole dataRes data

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const data = (name, age, work, ...names) => {
 };
 const dataRes = data("john", 30, "Help-Desk", "mike", "ben", "tim");
 
-if (dataRes instanceof Object && Array.isArray(dataRes.names || dataRes instanceof Array))
-  {
-  console.log(dataRes.names.splice(1, 2));
+if (
+  dataRes instanceof Object &&
+  Array.isArray(dataRes.names || dataRes instanceof Array)
+) {
+  const results = { ...dataRes };
+  console.log(results);
 } else {
   console.log(typeof dataRes);
 }
