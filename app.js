4f98a1e30b0a78be6e233716ca932496ca746305
Kamogelo <ekmogasoa@gmail.com>
2024-12-05 15:31:29 +0200

    added new logic into app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const data = (name, age, work) => {
+const data = (name, age, work, ...names) => {
   return {
     name,
     age,
     work,
+    names,
   };
+};
+const dataRes = data("john", 30, "Help-Desk", "mike", "ben", "tim");
+
+if (dataRes instanceof Object) {
+  console.log(dataRes.names.splice(1, 2));
+} else {
+  console.log(typeof dataRes);
 }
-const dataRes = data("john", 30, "Help-Desk");
-console.log(dataRes);
