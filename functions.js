422df6406adedbc8b98e597b26d6754c41ff6965
Kamogelo <ekmogasoa@gmail.com>
2024-12-05 14:23:35 +0200

    checked functions.js variable from age to userAge

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
-const age = 18;
+const userAge = 18;
 
-if (age >= 18) {
+if (userAge >= 18) {
   console.log(`welcome`);
 } else {
-  throw Error(`Can't enter your underage ${age}`);
+  throw Error(`Can't enter your underage ${userAge}`);
 }
