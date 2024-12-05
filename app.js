2f854c9d036da8241125200d48022e74eca6a7b4
Kamogelo <ekmogasoa@gmail.com>
2024-12-05 16:14:15 +0200

    fix: corrected logic for checking if dataRes is an object or array

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const data = (name, age, work, ...names) => {
 };
 const dataRes = data("john", 30, "Help-Desk", "mike", "ben", "tim");
 
-if (dataRes instanceof Object || dataRes instanceof Array) {
+if (dataRes instanceof Object && Array.isArray(dataRes.names || dataRes instanceof Array))
+  {
   console.log(dataRes.names.splice(1, 2));
 } else {
   console.log(typeof dataRes);
